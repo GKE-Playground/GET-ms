0e58d8f34de889673c47c0426ae02537f819916c
tungUAS <Tung.Trinh@senacor.com>
2023-11-17 22:10:03 +0100

    add posgresSQL

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import createError from "http-errors";
+import pg from "pg";
+const { Pool } = pg;
 
 import type {
   Express,
@@ -18,12 +20,34 @@ const app: Express = express();
 
 const PORT = config.get<string>("SERVICE_PORT") || 3000;
 
+// Create a MySQL connection pool
+const pool = new Pool({
+  user: "woo-backend",
+  host: "localhost", 
+  database: "integration-test",
+  password: "woo-backend", 
+  port: 5432,
+});
+
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("GET ms");
+});
+
+app.get("/getData", async (_req: Request, res: Response) => {
+  try {
+    // Run the select query using the pool
+    const queryResult = await pool.query("SELECT * FROM TODO");
+
+    // Send the query results as JSON
+    res.json(queryResult.rows);
+  } catch (error) {
+    console.error("Error running query", error);
+    res.status(500).json({ error: error });
+  }
 });
 
 app.use(function (_req: Request, _res: Response, next: NextFunction) {
