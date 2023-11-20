7366a42131789b90cacc7073a8191a3da5e53330
tungUAS <Tung.Trinh@senacor.com>
2023-11-21 00:14:15 +0100

    do something

--- src/app.ts
+++ src/app.ts
@@ -36,11 +36,11 @@ const DB_PASSWORD = config.get<string>("DB_PASS");
 const DB_PORT = config.get<number>("DB_PORT");
 
 const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  database: DB_NAME,
-  password: DB_PASSWORD,
-  port: DB_PORT,
+  user: process.env["DB_USER"],
+  host: process.env["DB_HOST"],
+  database: process.env["DB_NAME"],
+  password: process.env["DB_PASS"],
+  port: Number(process.env["DB_PORT"]),
 });
 
 logger.info("Database Pool Configuration:", pool);
