cee0e12cdf0af388663bb01ef877a6dc5d1076e0
tungUAS <Tung.Trinh@senacor.com>
2023-11-21 23:20:02 +0100

    lets try

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import bodyParser from "body-parser";
-import config from "config";
 import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import createError from "http-errors";
 import pg from "pg";
-import winston from "winston";
 const { Pool } = pg;
 
 import type {
@@ -15,42 +13,32 @@ import type {
   Response,
 } from "express";
 
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.simple(),
-  transports: [ new winston.transports.Console() ],
-});
-
 dotenv.config();
 
 const app: Express = express();
 
-logger.info("Environment Configurations:", process.env);
+const PORT = process.env["SERVICE_PORT"] || 3000;
 
-const PORT = config.get<string>("SERVICE_PORT") || 3000;
-
-const DB_USER = config.get<string>("DB_USER");
-const DB_HOST = config.get<string>("DB_HOST");
-const DB_NAME = config.get<string>("DB_NAME");
-const DB_PASSWORD = config.get<string>("DB_PASS");
-const DB_PORT = config.get<number>("DB_PORT");
+const DB_USER = process.env["DB_USER"];
+const DB_HOST = process.env["DB_HOST"];
+const DB_NAME = process.env["DB_NAME"];
+const DB_PASSWORD = process.env["DB_PASS"];
+const DB_PORT = process.env["DB_PORT"];
 
 const pool = new Pool({
   user: DB_USER,
   host: DB_HOST,
   database: DB_NAME,
   password: DB_PASSWORD,
-  port: DB_PORT,
+  port: Number(DB_PORT),
 });
-
-logger.info("Database Pool Configuration:", pool);
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (_req: Request, res: Response) => {
-  res.send("GET ms");
+  res.send("GET ms. Hello World!");
 });
 
 app.get("/getData", async (_req: Request, res: Response) => {
